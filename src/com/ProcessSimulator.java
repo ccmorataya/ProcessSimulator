94601be3c9d30de04e7a72787f228c98a2d0ae80
Cristian Morataya <cris.morataya@gmail.com>
2017-05-30 21:16:20 -0600

    Initial commit

---
+++ src/com/ProcessSimulator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author ba
+ */
+public class ProcessSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My frame");
+	frame.setSize(800, 600);
+	frame.setVisible(true);
+    }
+    
+}
