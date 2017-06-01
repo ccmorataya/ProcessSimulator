5018667ba4b3c154342f20d214aaf18b70dfce5e
Cristian Morataya <cris.morataya@gmail.com>
2017-05-31 22:39:52 -0600

    Add .gitignore, gridLoyout and logic components

--- src/com/ProcessSimulator.java
+++ src/com/ProcessSimulator.java
@@ -5,6 +5,10 @@
  */
 package com;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.*;
 
 /**
@@ -12,14 +16,49 @@ import javax.swing.*;
  * @author ba
  */
 public class ProcessSimulator {
-
+	static ArrayList<String> processArray = new ArrayList<>();
+	static ArrayList<String> burstTimeArray = new ArrayList<>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My frame");
+	
+	GridLayout gdLayout = new GridLayout(4, 3, 10, 10);
+	    
+	String[] algorithmsString = { "RR", "FCFS", "SJF"};
+	JComboBox algorithmsList = new JComboBox(algorithmsString);
+	
+	JTextField txtProcess = new JTextField("Proceso:");
+	JTextField txtBurstTime = new JTextField("Burst Time:");
+	JButton btnAddProcess = new JButton("Agregar");
+	
+	JFrame frame = new JFrame("My frame");
+	frame.setDefaultCloseOperation(3);
+	frame.setLayout(gdLayout);
+	frame.add(new JLabel(""));
+	frame.add(algorithmsList);
+	frame.add(new JLabel(""));
+	frame.add(txtProcess);
+	frame.add(txtBurstTime);
+	frame.add(btnAddProcess);
+	frame.add(new JLabel(""));
+	frame.add(new JLabel(""));
+	frame.add(new JLabel(""));
+	frame.add(new JLabel(""));
+	frame.add(new JLabel(""));
 	frame.setSize(800, 600);
 	frame.setVisible(true);
+	
+	btnAddProcess.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			processArray.add(txtProcess.getText());
+			burstTimeArray.add(txtBurstTime.getText());
+			txtProcess.setText("");
+			txtBurstTime.setText("");
+			System.out.println(processArray + " " + burstTimeArray);
+		}
+	});
     }
     
 }
